ccb174dc9d38d649093a8436ea2f4964811555c8
Tiesen <ttien56906@gmail.com>
2024-05-10 19:00:40 +0700

    chore: owo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
 
 const Page: NextPage = () => {
   return (
-    <div className="flex h-[70dvh] flex-col items-center justify-center gap-8">
+    <div className="flex h-[60dvh] flex-col items-center justify-center gap-8">
       <Typography variant="h1">EGG!</Typography>
       <Typography variant="h2" className="border-none">
-        404 | Page Not Found
+        404 | Page Not Found OwO
       </Typography>
 
       <Button
-        className="bg-gradient-yuki text-xl font-bold text-white transition-transform hover:scale-105"
+        className="bg-gradient-yuki text-xl font-bold text-white transition-transform ease-linear hover:scale-105"
         size="lg"
         asChild
       >
--- components/home/button-group.tsx
+++ components/home/button-group.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
 export const ButtonGroup: React.FC = () => (
-  <div className="mt-4 flex items-center justify-center gap-8 [&>*]:text-xl [&>*]:font-bold [&>*]:transition-all [&>*]:ease-linear">
-    <Button className="bg-gradient-yuki text-white hover:scale-105" asChild>
+  <div className="mt-4 flex grid grid-cols-2 gap-4 *:text-xl *:font-bold *:text-white *:transition-transform *:ease-linear hover:*:scale-105">
+    <Button className="bg-gradient-yuki" asChild>
       <Link href="/#contact-form">Contact Me</Link>
     </Button>
 
     <Button
-      className="animate-shimmer bg-[linear-gradient(110deg,var(--from),45%,var(--to),55%,var(--from))] bg-[length:200%_100%] text-white hover:scale-105"
+      className="animate-shimmer bg-[linear-gradient(110deg,var(--from),45%,var(--to),55%,var(--from))] bg-[length:200%_100%]"
       asChild
     >
       <Link href="https://youtu.be/dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
