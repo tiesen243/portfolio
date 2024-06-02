03bb3e9561ccda068ef10043dec7828a5ce15db3
Tiesen <ttien56906@gmail.com>
2024-06-02 23:08:04 +0700

    chore: disable load

--- components/view.tsx
+++ components/view.tsx
@@ -1,6 +1,17 @@
-import { getViews } from '@/lib/actions'
+'use client'
 
-export const View: React.FC<{ slug: string }> = async ({ slug }) => {
-  const views = await getViews(slug)
+import { getViews } from '@/lib/actions'
+import { useEffect, useState } from 'react'
+
+export const View: React.FC<{ slug: string }> = ({ slug }) => {
+  const [views, setViews] = useState<string>('0')
+  useEffect(() => {
+    getViews(slug)
+      .then((views) => {
+        if (+views >= 1000) setViews(`${views.slice(0, -3)}k`)
+        else setViews(views)
+      })
+      .catch(() => setViews('0'))
+  }, [slug])
   return <>{views} views</>
 }
