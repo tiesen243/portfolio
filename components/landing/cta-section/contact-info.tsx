296d0df28e0de56e77e971acdc8de17983428aa9
tiesen243 <ttien56906@gmail.com>
2024-07-06 19:21:37 +0700

    style: change sth

--- components/landing/cta-section/contact-info.tsx
+++ components/landing/cta-section/contact-info.tsx
@@ -4,7 +4,7 @@ export const ContactInfo: React.FC = () => (
   <ul className="flex flex-col gap-2 pt-4 md:pt-8">
     {siteConfig.contact.map((c) => (
       <li key={c.label} className="flex items-center gap-2">
-        <div className="aspect-square rounded bg-secondary p-2">
+        <div className="aspect-square rounded-lg bg-primary/10 p-2">
           <c.icon className="text-yuki" />
         </div>
 
