c3765f4c43d3de1bd678b7f371642062ed7001fb
tiesen243 <ttien56906@gmail.com>
2024-07-30 06:36:41 +0700

    feat(sitemap): add project routes to sitemap
    - Import project data from '@/lib/data.json'
    - Map project routes and add them to the sitemap
    - Include project routes in the fetched routes array

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,19 +9,24 @@ interface Route {
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const projects = await import('@/lib/data.json').then((data) => data.projects)
   const routesMap = ['', 'projects', 'blog'].map((route) => ({
     url: `${getBaseUrl()}/${route}`,
     lastModified: new Date().toISOString(),
   }))
 
   const blogRoutes = getPages().map((page) => ({
-    url: `${getBaseUrl()}/${page.url}`,
+    url: `${getBaseUrl()}${page.url}`,
     lastModified: page.data.date.toISOString(),
+  }))
+  const projectsRoutes = projects.map((project) => ({
+    url: `${getBaseUrl()}/projects/${project.slug}`,
+    lastModified: new Date().toISOString(),
   }))
 
   let fetchedRoutes: Route[] = []
   try {
-    fetchedRoutes = (await Promise.all([blogRoutes])).flat()
+    fetchedRoutes = (await Promise.all([blogRoutes, projectsRoutes])).flat()
   } catch (error) {
     throw JSON.stringify(error, null, 2)
   }
