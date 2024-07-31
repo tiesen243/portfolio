cd1662c6bfdd6b5d5a555566ad0a8aeeb5c1162f
tiesen243 <ttien56906@gmail.com>
2024-08-01 05:31:37 +0700

    feat(blog): add tag filtering and navigation
    - Added tag filtering functionality to the blog page.
    - Implemented navigation links for tags.
    - Updated the blog layout to include a tag navigation bar.
    - Enhanced the blog card rendering based on selected tags.

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -55,7 +55,7 @@ const Page: NextPage<Props> = async ({ params: { slug } }) => {
           {page.data.tags.map((tag) => (
             <li
               key={tag}
-              className="inline-block whitespace-nowrap rounded-full bg-primary px-3 py-1 text-sm font-semibold text-primary-foreground hover:bg-primary/80"
+              className="inline-block cursor-pointer whitespace-nowrap rounded-full bg-primary px-3 py-1 text-sm font-semibold text-primary-foreground hover:bg-primary/80"
             >
               {tag}
             </li>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,47 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 import { BlogCard } from '@/components/blog-card'
 import { getPages } from '@/content'
 
-const Page: NextPage = () => {
+interface Props {
+  searchParams: { tag?: string }
+}
+
+const Page: NextPage<Props> = ({ searchParams }) => {
   const blogs = getPages()
+  const tags = [...new Set(blogs.map((blog) => blog.data.tags).flat())]
+
+  const renderBlogs = searchParams.tag
+    ? blogs.filter((blog) => blog.data.tags.includes(searchParams.tag ?? ''))
+    : blogs
 
   return (
-    <main className="container my-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {blogs.map((blog) => (
-        <BlogCard key={blog.url} blog={blog} />
-      ))}
+    <main className="container my-4">
+      <nav className="mb-4 flex gap-4">
+        <Link
+          href="/blog"
+          className={searchParams.tag ? 'text-muted-foreground hover:underline' : ''}
+        >
+          All
+        </Link>
+
+        {tags.map((tag) => (
+          <Link
+            key={tag}
+            href={`/blog?tag=${tag}`}
+            className={searchParams.tag === tag ? '' : 'text-muted-foreground hover:underline'}
+          >
+            {tag}
+          </Link>
+        ))}
+      </nav>
+
+      <section className=" grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {renderBlogs.map((blog) => (
+          <BlogCard key={blog.url} blog={blog} />
+        ))}
+      </section>
     </main>
   )
 }
