ffd7d6f174513f65fe44d16e9bb8cbc71ff7aadb
tiesen243 <ttien56906@gmail.com>
2024-08-06 07:53:51 +0700

    feat: add background image to OpenGraph image component
    - Added a background image to the OpenGraph image component using inline
      styles.
    - Updated the package.json to include the "sharp" library version 0.33.4.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -18,7 +18,12 @@ export const contentType = 'image/png'
 export default async function Image() {
   return new ImageResponse(
     (
-      <div tw="w-full h-full flex justify-center items-center bg-black text-white">
+      <div
+        tw="w-full h-full flex justify-center items-center bg-black text-white"
+        style={{
+          backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='%2318181b'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
+        }}
+      >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={`${getBaseUrl()}/images/tiesen.png`} alt={alt} tw="w-5/6" />
       </div>
