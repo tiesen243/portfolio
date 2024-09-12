ab18ae45f5af9d079a42bf775ebdd484a62f8bf4
tiesen243 <ttien56906@gmail.com>
2024-09-12 07:35:35 +0700

    fix: adjust padding and order of elements in blog TOC
    - Adjusted padding for desktop and mobile TOC components
    - Reordered elements in mobile TOC for better UX
    - Corrected minor typos in the Arch Linux setup guide

--- app/blog/_components/blog-toc/mobile.tsx
+++ app/blog/_components/blog-toc/mobile.tsx
@@ -13,30 +13,25 @@ export const Mobile: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <section className="sticky inset-0 z-50 w-svw select-none border-b bg-background/70 py-2 shadow-lg backdrop-blur-xl">
       <div className="container flex items-center justify-between gap-4">
-        <Link href="/" className="text-lg font-bold">
-          Tiesen | Blog
-        </Link>
-
-        <div className="flex items-center gap-4">
-          <Link href="/blog" className="text-muted-foreground hover:text-foreground">
-            All posts
-          </Link>
-          <Button onClick={handleToggle} variant="ghost" size="sm" className="z-20">
-            On this page <MenuIcon className="ml-2" />
-          </Button>
-        </div>
+        <Button onClick={handleToggle} variant="ghost" size="sm" className="z-20">
+          <MenuIcon className="mr-2" /> On this page
+        </Button>
 
         {isOpen && <div className="fixed inset-0 z-10 h-dvh w-svw" onClick={handleToggle} />}
 
         <div
           onClick={handleToggle}
           className={cn(
-            'fixed right-8 top-14 z-20 w-fit rounded-lg border bg-background/70 p-4 pl-0 backdrop-blur-xl transition-all ease-linear',
-            isOpen ? 'translate-x-0 opacity-100' : 'pointer-events-none translate-x-20 opacity-0',
+            'fixed left-8 top-14 z-20 w-fit min-w-60 rounded-lg border bg-card p-4 pl-2 transition-all ease-linear',
+            isOpen ? 'translate-y-0 opacity-100' : 'pointer-events-none translate-y-10 opacity-0',
           )}
         >
           {children}
         </div>
+
+        <Link href="/blog" className="text-muted-foreground hover:text-foreground">
+          All posts
+        </Link>
       </div>
     </section>
   )
--- app/blog/_components/blog-toc/toc.tsx
+++ app/blog/_components/blog-toc/toc.tsx
@@ -4,9 +4,9 @@ import { createId } from '@/components/mdx'
 import { type Post } from '@/lib/actions/mdx'
 
 export const Toc: React.FC<{ toc: Post['toc'] }> = ({ toc }) => (
-  <ul className="list-none pl-4">
+  <ul className="list-none pl-2 xl:text-lg">
     {toc.map((header) => (
-      <li key={header.title} className="text-lg">
+      <li key={header.title}>
         <Link href={`#${createId(header.title)}`} className="hover:underline">
           {header.title}
         </Link>
