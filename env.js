7fbbdc0ecc75e42bc7bb1200d53469506ce93ec0
tiesen243 <ttien56906@gmail.com>
2024-09-18 05:29:32 +0700

    feat(env): add RESEND_KEY and update dependencies
    - Added RESEND_KEY to the server and runtime environment configurations
    - Updated package.json to include "resend" and "sharp" dependencies

--- env.js
+++ env.js
@@ -10,6 +10,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    RESEND_KEY: z.string(),
     VERCEL_PROJECT_PRODUCTION_URL: z.string().optional(),
   },
 
@@ -28,6 +29,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    RESEND_KEY: process.env.RESEND_KEY,
     VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
