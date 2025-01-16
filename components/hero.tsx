c43ef6db62eef7aa5d85ea7b37d1db46dcad66b8
tiesen243 <ttien56906@gmail.com>
2025-01-16 21:43:03 +0700

    style: add shadow for hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,11 +10,16 @@ export const Hero: React.FC = () => (
     }}
     className="flex flex-col items-center pb-8 text-center"
   >
-    <Image src={Logo} alt="Logo" className="mb-6 w-full max-w-[500px] px-4" priority />
+    <Image
+      src={Logo}
+      alt="Logo"
+      className="mb-6 w-full max-w-[500px] px-4 shadow-lg"
+      priority
+    />
 
     <p className="mb-6 h-fit text-pretty p-2 text-lg text-muted-foreground md:max-w-[80%] md:text-xl">
       I'm Tran Tien, a Weeb Developer who loves to
-      <b className="font-medium text-fd-foreground"> code and watch anime</b>.
+      <b className="font-medium text-yuki"> code and watch anime</b>.
     </p>
   </div>
 )
