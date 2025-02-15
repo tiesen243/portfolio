23f470e42974d3e5c206f908565af366d1f2f11a
tiesen243 <ttien56906@gmail.com>
2025-02-16 02:13:08 +0700

    fix: grid overlay

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,10 +6,10 @@ export default function HomePage() {
   return (
     <>
       <div
-        className="absolute inset-x-0 top-[200px] h-[250px] max-md:hidden"
+        className="absolute inset-x-0 top-[200px] h-[250px] opacity-10 max-md:hidden"
         style={{
           background:
-            'repeating-linear-gradient(to right, hsl(var(--primary)/.1),hsl(var(--primary)/.1) 1px,transparent 1px,transparent 50px), repeating-linear-gradient(to bottom, hsl(var(--primary)/.1),hsl(var(--primary)/.1) 1px,transparent 1px,transparent 50px)',
+            'repeating-linear-gradient(to right, var(--color-primary),var(--color-primary) 1px,transparent 1px,transparent 50px), repeating-linear-gradient(to bottom, var(--color-primary),var(--color-primary) 1px,transparent 1px,transparent 50px)',
         }}
       />
       <main className="relative container pt-4 lg:pt-16">
