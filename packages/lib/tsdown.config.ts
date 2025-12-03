fa3101c605a143261129c16b9f7ae1b9227bebd6
tiesen243 <ttien56906@gmail.com>
2025-12-04 01:11:34 +0700

    chore: minor changes

--- packages/lib/tsdown.config.ts
+++ packages/lib/tsdown.config.ts
@@ -4,16 +4,5 @@ export default defineConfig({
   entry: ['./src/*.ts'],
   dts: true,
   shims: true,
-  exports: {
-    customExports() {
-      return {
-        '.': './dist/index.mjs',
-        './*': {
-          default: './dist/*.mjs',
-          types: './dist/*.d.mts',
-        },
-        './package.json': './package.json',
-      }
-    },
-  },
+  exports: true,
 })
