aa4b62726d71d43acc0adeaa310e38c50e2cec51
tiesen243 <ttien56906@gmail.com>
2025-12-25 03:26:02 +0700

    chore: minor changes

--- packages/ui/src/hooks/use-mounted.tsx
+++ packages/ui/src/hooks/use-mounted.tsx
@@ -4,7 +4,6 @@ export const useMounted = () => {
   const [isMounted, setIsMounted] = React.useState(false)
 
   React.useEffect(() => {
-    // eslint-disable-next-line @eslint-react/hooks-extra/no-direct-set-state-in-use-effect
     setIsMounted(true)
   }, [])
 
--- packages/validators/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from '@yuki/eslint-config/base'
-import { defineConfig } from 'eslint/config'
-
-export default defineConfig(
-  {
-    ignores: ['dist/**'],
-  },
-  ...baseConfig,
-)
