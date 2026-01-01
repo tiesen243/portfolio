049c0a517c7ae4887219255004c6eda48db3bdef
tiesen243 <ttien56906@gmail.com>
2026-01-01 19:10:48 +0700

    chore: bump deps

--- packages/ui/src/components/typography.tsx
+++ packages/ui/src/components/typography.tsx
@@ -7,26 +7,26 @@ import { cva } from 'class-variance-authority'
 import { cn } from '@/utils'
 
 const typographyVariants = cva('text-base font-normal', {
-  defaultVariants: {
-    variant: 'p',
-  },
   variants: {
     variant: {
-      blockquote:
-        'my-2 inline-flex border-l-2 pl-6 italic before:content-["“"] after:content-["”"]',
-      caption: 'block text-sm tracking-wide',
-      code: 'relative w-fit border border-accent rounded-sm bg-accent/40 text-accent-foreground px-[0.3rem] py-[0.2rem] font-mono text-sm font-medium',
       h1: 'my-8 scroll-m-20 text-4xl font-extrabold tracking-tight text-balance',
       h2: 'my-5 scroll-m-20 text-3xl font-bold tracking-tight text-balance first:mt-0',
       h3: 'my-4 scroll-m-20 text-2xl font-semibold tracking-tight text-balance',
       h4: 'my-3 scroll-m-20 text-xl font-semibold tracking-tight text-balance',
       h5: 'my-2.5 scroll-m-20 text-lg font-medium tracking-tight text-balance',
       h6: 'my-2 scroll-m-20 text-base font-medium tracking-tight text-balance',
-      ol: 'my-4 ml-6 list-decimal text-base [&>li]:mt-2 [&>li]:first:mt-0',
       p: 'leading-7 text-pretty [&:not(:first-child)]:mt-2',
-      small: 'text-sm leading-none font-medium',
       ul: 'my-4 ml-6 list-disc text-base [&>li]:mt-2 [&>li]:first:mt-0',
+      ol: 'my-4 ml-6 list-decimal text-base [&>li]:mt-2 [&>li]:first:mt-0',
+      small: 'text-sm leading-none font-medium',
+      blockquote:
+        'my-2 inline-flex border-l-2 pl-6 italic before:content-["“"] after:content-["”"]',
+      caption: 'block text-sm tracking-wide',
+      code: 'relative w-fit border border-accent rounded-sm bg-accent/40 text-accent-foreground px-[0.3rem] py-[0.2rem] font-mono text-sm font-medium',
     },
+  },
+  defaultVariants: {
+    variant: 'p',
   },
 })
 
