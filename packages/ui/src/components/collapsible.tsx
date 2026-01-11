59638d7cd95acf3231ef85448b561626634fa39b
tiesen243 <ttien56906@gmail.com>
2026-01-11 13:21:17 +0700

    chore: minor changes

--- packages/ui/src/components/collapsible.tsx
+++ packages/ui/src/components/collapsible.tsx
@@ -1,20 +1,20 @@
-"use client"
+'use client'
 
-import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible"
+import { Collapsible as CollapsiblePrimitive } from '@base-ui/react/collapsible'
 
 function Collapsible({ ...props }: CollapsiblePrimitive.Root.Props) {
-  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
+  return <CollapsiblePrimitive.Root data-slot='collapsible' {...props} />
 }
 
 function CollapsibleTrigger({ ...props }: CollapsiblePrimitive.Trigger.Props) {
   return (
-    <CollapsiblePrimitive.Trigger data-slot="collapsible-trigger" {...props} />
+    <CollapsiblePrimitive.Trigger data-slot='collapsible-trigger' {...props} />
   )
 }
 
 function CollapsibleContent({ ...props }: CollapsiblePrimitive.Panel.Props) {
   return (
-    <CollapsiblePrimitive.Panel data-slot="collapsible-content" {...props} />
+    <CollapsiblePrimitive.Panel data-slot='collapsible-content' {...props} />
   )
 }
 
