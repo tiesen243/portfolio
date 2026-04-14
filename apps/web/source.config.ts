e01c2a7a12582283f8865fdc58685cab472c4d29
tiesen243 <ttien56906@gmail.com>
2026-04-15 03:21:20 +0700

    feat: update codeblock theme

--- apps/web/source.config.ts
+++ apps/web/source.config.ts
@@ -21,6 +21,11 @@ export const docs = defineDocs({
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    rehypeCodeOptions: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark-default',
+      },
+    },
   },
 })
