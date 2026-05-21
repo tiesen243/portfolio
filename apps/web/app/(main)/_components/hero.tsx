af3cd77ee481993b8fa48dc9cfa4d87dcb3fe651
tiesen243 <ttien56906@gmail.com>
2026-05-21 10:45:49 +0700

    chore: minor changes

--- apps/web/app/(main)/_components/hero.tsx
+++ apps/web/app/(main)/_components/hero.tsx
@@ -14,25 +14,41 @@ export function HeroSection() {
   return (
     <section
       id='hero'
-      className='container flex min-h-[calc(100dvh-3.5rem)] max-w-2xl flex-col items-center justify-center'
+      className='relative container flex min-h-[calc(100dvh-3.5rem)] flex-col items-center justify-center'
     >
       <h2 className='sr-only'>Hero section</h2>
 
-      <code className='mb-6 text-primary'>&quot;use portfolio&quot;</code>
+      <div
+        className='pointer-events-none absolute inset-0 -z-10 h-full w-full'
+        style={{
+          backgroundSize: '48px 48px',
+          backgroundImage: `
+            linear-gradient(to right, var(--color-secondary) 1px, transparent 1px),
+            linear-gradient(to bottom, var(--color-secondary) 1px, transparent 1px)
+          `,
+
+          maskImage:
+            'radial-gradient(circle at center, var(--color-background) 0%, transparent 100%)',
+          WebkitMaskImage:
+            'radial-gradient(circle at center, var(--color-background) 0%, transparent 100%)',
+        }}
+      />
+
+      <code className='text-primary'>&quot;use portfolio&quot;</code>
 
       <Image
         src={Logotype}
         alt='Tiesen'
         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-        className='mx-auto h-auto object-cover'
+        className='mx-auto my-8 h-auto w-full max-w-4xl object-cover'
       />
 
-      <Typography className='mt-6 max-w-xl text-center text-pretty [&_span]:text-primary'>
+      <Typography className='max-w-xl text-center text-pretty [&_span]:text-primary'>
         You can&apos;t change your <span>past</span>, but luckily you can still
         destroy your <span>future</span> OwO.
       </Typography>
 
-      <nav className='mt-8 flex w-full flex-col gap-4'>
+      <nav className='mt-6 flex w-full max-w-2xl flex-col gap-4'>
         {navs.map((nav) => (
           <Link
             key={nav.label}
